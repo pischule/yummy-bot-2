a085a44a8a0f80219461ea00137a5a678b93c809
Maksim Pischulenok <pischule@outlook.com>
2022-04-20 09:34:14 +0300

    refactoring

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,30 +1,71 @@
 import styles from "./App.module.css";
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
+
+import { baseUrl } from "./const";
 
 import MenuScreen from "./components/Menu/MenuScreen";
 import ConfirmScreen from "./components/Confirm/ConfirmScreen";
 import DoneScreen from "./components/DoneScreen/DoneScreen";
 
-
 function App() {
   const [screen, setScreen] = useState(0);
   const [items, setItems] = useState([]);
+  const [title, setTitle] = useState("Меню не доступно");
 
-  const switchToConfirm = (index, items) => {
-    setScreen(1);
-    setItems(items);
-  }
+  useEffect(() => {
+    async function fetchData() {
+      const result = await fetch(`${baseUrl}/menu`);
+      const json = await result.json();
+      const itemsWithQuantity = json.items.map((item) => {
+        return {
+          name: item,
+          quantity: 0,
+        };
+      });
+      setItems(itemsWithQuantity);
+      setTitle(json.title);
+    }
+    fetchData();
+  }, []);
+
+  const updateQuantity = (name, count) => {
+    const newItems = items.map((item) => {
+      if (item.name === name) {
+        item.quantity = count;
+      }
+      return item;
+    });
+    setItems(newItems);
+  };
+
+  const switchToConfirem = () => {
+    const newItems = items.filter((item) => item.quantity > 0);
+    if (newItems.length > 0) {
+      setScreen(1);
+    }
+  };
 
   const switchToDone = () => {
     setScreen(2);
-  }
+  };
+
+  const orderedItems = items.filter((item) => item.quantity > 0);
 
   return (
     <div className={styles.app}>
-      {screen === 0 && <MenuScreen switchToConfirm={switchToConfirm} />}
-      {screen === 1 && <ConfirmScreen switchToDone={switchToDone} items={items}/>}
-      {screen === 2 && <DoneScreen/>}
+      {screen === 0 && (
+        <MenuScreen
+          updateQuantity={updateQuantity}
+          handleClick={switchToConfirem}
+          items={items}
+          title={title}
+        />
+      )}
+      {screen === 1 && (
+        <ConfirmScreen switchToDone={switchToDone} items={orderedItems} />
+      )}
+      {screen === 2 && <DoneScreen />}
     </div>
   );
 }
--- frontend/src/components/Menu/MenuScreen.js
+++ frontend/src/components/Menu/MenuScreen.js
@@ -1,59 +1,15 @@
-import { useState, useEffect } from "react";
-
 import LargeButton from "../UI/LargeButton";
 import ItemList from "./ItemsList";
 
 import styles from "./Menu.module.css";
 
-import { baseUrl } from "../../const";
-
 function MenuScreen(props) {
-  const [items, setItems] = useState([]);
-  const [title, setTitle] = useState("Меню не доступно");
-
-  useEffect(() => {
-    async function fetchData() {
-      const result = await fetch(`${baseUrl}/menu`);
-      const json = await result.json();
-      const itemsWithQuantity = json.items.map((item) => {
-        return {
-          name: item,
-          quantity: 0,
-        };
-      });
-      setItems(itemsWithQuantity);
-      setTitle(json.title);
-    }
-    fetchData();
-  }, []);
-
-  const updateQuantity = (name, count) => {
-    const newItems = items.map((item) => {
-      if (item.name === name) {
-        item.quantity = count;
-      }
-      return item;
-    });
-    setItems(newItems);
-  };
-
-  const handleClick = () => {
-    const newItems = items.filter((item) => item.quantity > 0);
-    if (newItems.length > 0) {
-      setItems(newItems);
-      props.switchToConfirm(
-        1,
-        items.filter((item) => item.quantity > 0)
-      );
-    }
-  };
-
   return (
     <div className="menu">
-      <h1 className={styles.h1}>{title}</h1>
-      <ItemList items={items} updateQuantity={updateQuantity} />
-      {items.length > 0 && (
-        <LargeButton onClick={handleClick}>Заказать</LargeButton>
+      <h1 className={styles.h1}>{props.title}</h1>
+      <ItemList items={props.items} updateQuantity={props.updateQuantity} />
+      {props.items.length > 0 && (
+        <LargeButton onClick={props.handleClick}>Заказать</LargeButton>
       )}
     </div>
   );
